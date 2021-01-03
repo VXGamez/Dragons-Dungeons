47c3f8fd6685b95c158352a8da51ab3126de677c
VXGamez <victor.xirau@students.salle.url.edu>
2021-01-03 21:13:56 +0100

    finishDia1

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ function nuevaPartida(){
           imagen.src = avatars[obj.image-1];
           imagen.className = 'characterImage';
           imagen.alt = 'characterImage';
-          imagen.style.height='300px';
+          imagen.style.height='250px';
           newInfo.appendChild(imagen);
           var node = document.createElement("div");
           node.className = 'life';
@@ -96,6 +96,7 @@ function nuevaPartida(){
           ataque.style.fontSize = '30px';
           ataque.style.color = 'white';
           ataque.style.marginTop = '5%';
+          ataque.style.marginLeft = '25%';
 
           diversiong.appendChild(ataque);
 
@@ -103,18 +104,38 @@ function nuevaPartida(){
           img.src =  espadas;
           img.alt = 'espadas';
           img.style.width = '10%';
-          img.style.float = 'left';
+          img.style.marginLeft = '5%';
 
           diversiong.appendChild(img);
 
+          diversiong.style.display = 'flex';
+          diversiong.style.flexDirection = 'row';
+
           dire.appendChild(diversiong);
+          
+          var diversiong2 = document.createElement("div");
           
           var defensa = document.createElement("p");
           defensa.innerText = 'Defensa: ' + obj.defense;
           defensa.style.fontFamily = 'Lucida Console,Lucida Sans Typewriter,monaco,Bitstream Vera Sans Mono,monospace';
           defensa.style.fontSize = '30px';
           defensa.style.color = 'white';
-          dire.appendChild(defensa);
+          defensa.style.marginLeft = '25%';
+
+          diversiong2.appendChild(defensa);
+
+          var img2 = document.createElement("img");
+          img2.src = escudo;
+          img2.alt = 'escudo';
+          img2.style.width = '10%';
+          img2.style.marginLeft = '5%';
+        
+          
+          diversiong2.style.display = 'flex';
+          diversiong2.style.flexDirection = 'row';
+
+          diversiong2.appendChild(img2);
+          dire.appendChild(diversiong2);
 
         })
         .catch(function (err) {
@@ -252,28 +273,70 @@ function App() {
         <div className="stats" id="stats"></div>
         <div className="objetos">
           <div className="obj1">
-            <div className="imgObj1"></div>
-            <div className="infoObj1"></div>
+            <div className="imgObj1">
+              <img src="https://raw.githubusercontent.com/VXGamez/P2-PW/main/src/assets/objects/axe.svg?token=ALPT6YAHZPFG6CIJQ5ZXXY277NEO2" alt="obj1" width="50px"/>
+              <h3>Q</h3>
+            </div>
+            <div className="infoObj1">
+              <h6>Hachas del destino</h6>
+              <h6>Ataque : X</h6>
+              <h6>Defensa : X</h6>
+            </div>
           </div>
           <div className="obj2">
-            <div className="imgObj2"></div>
-            <div className="infoObj2"></div>
+            <div className="imgObj2">
+              <img src="https://raw.githubusercontent.com/VXGamez/P2-PW/main/src/assets/objects/kunai.svg?token=ALPT6YE3XKRBYDZIKYFGFPS77NET6" alt="obj2" width="50px"/>
+              <h3>W</h3>
+            </div>
+            <div className="infoObj2">
+              <h6>Kunai Represor</h6>
+              <h6>Ataque : X</h6>
+              <h6>Defensa : X</h6>
+            </div>
           </div>
           <div className="obj3">
-            <div className="imgObj3"></div>
-            <div className="infoObj3"></div>
+            <div className="imgObj3">
+              <img src="https://raw.githubusercontent.com/VXGamez/P2-PW/main/src/assets/objects/spear.svg?token=ALPT6YCQJB627XIZ6Q2COTC77NEUS" alt="obj3" width="50px"/>
+              <h3>E</h3>
+            </div>
+            <div className="infoObj3">
+              <h6>Lanza Letal</h6>
+              <h6>Ataque : X</h6>
+              <h6>Defensa : X</h6>
+            </div>
           </div>
           <div className="obj4">
-            <div className="imgObj4"></div>
-            <div className="infoObj4"></div>
+            <div className="imgObj4">
+              <img src="https://raw.githubusercontent.com/VXGamez/P2-PW/main/src/assets/objects/weapon.svg?token=ALPT6YHHTINJ635FRSINDP277NEVE" alt="obj4" width="50px"/>
+              <h3>R</h3>
+            </div>
+            <div className="infoObj4">
+              <h6>Garras del Inmortal</h6>
+              <h6>Ataque : X</h6>
+              <h6>Defensa : X</h6>
+            </div>
           </div>
           <div className="obj5">
-            <div className="imgObj5"></div>
-            <div className="infoObj5"></div>
+            <div className="imgObj5">
+              <img src="https://raw.githubusercontent.com/VXGamez/P2-PW/main/src/assets/objects/attackPotion.svg?token=ALPT6YDY2XSYIMEV7M3JLC277NEB2" alt="obj5"width="50px"/>
+              <h3>T</h3>
+            </div>
+            <div className="infoObj5">
+              <h6>Poción de Sangre</h6>
+              <h6>Ataque : X</h6>
+              <h6>Defensa : X</h6>
+            </div>
           </div>
           <div className="obj6">
-            <div className="imgObj6"></div>
-            <div className="infoObj6"></div>
+            <div className="imgObj6">
+              <img src="https://raw.githubusercontent.com/VXGamez/P2-PW/main/src/assets/objects/defensePotion.svg?token=ALPT6YCLUDVO7BEY3JEG6CK77NEFI" alt="obj5" width="50px"/>
+              <h3>Y</h3>
+            </div>
+            <div className="infoObj6">
+              <h6>Poción de Corrupción</h6>
+              <h6>Ataque : X</h6>
+              <h6>Defensa : X</h6>
+            </div>
           </div>
           <div className="explicacion"></div>
         </div>
