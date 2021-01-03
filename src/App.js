ef3209a12710ed3cc8994bb180a3eeba422d5ec6
VXGamez <victor.xirau@students.salle.url.edu>
2021-01-03 19:30:30 +0100

    assetUpload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 //import logo from './logo.svg';
-import { logo, up, left, down, right,vidaLogo } from './assets';
+import { logo, up, left, down, right,vidaLogo, espadas, escudo } from './assets';
 import { avatars } from './assets/avatars';
 import Button from 'react-bootstrap/Button'
+import ProgressBar from 'react-bootstrap/ProgressBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -47,13 +48,12 @@ function makeRequest (method, url) {
 
 
 function nuevaPartida(){
-  if(constantes.PLAYER_INFO.player_token === "" && constantes.PLAYER_INFO.player_token === "" ){
+  if(constantes.PLAYER_INFO.player_token === "" && constantes.PLAYER_INFO.security_code === "" ){
     var nom = prompt('Introduce el nombre de tu jugador:', '');
   if(nom!=null){
     makeRequest('GET', 'http://battlearena.danielamo.info/api/spawn/'+constantes.TOKEN+'/'+nom)
     .then(function (datums) {
         var obj = JSON.parse(datums);
-        console.log(obj);
         constantes.PLAYER_INFO.player_token = obj.token;
         constantes.PLAYER_INFO.security_code = obj.code;
         console.log('Jugador Creado!\nToken: ' + constantes.PLAYER_INFO.player_token + '\nCodigo de Seguridadad: ' + constantes.PLAYER_INFO.security_code);
@@ -63,30 +63,58 @@ function nuevaPartida(){
           var obj = JSON.parse(datums);
           var newInfo = document.createElement("div");
           newInfo.id = 'informasion';
-          newInfo.style.backgroundColor = 'rgba(255, 255, 255, 0.365)';
+          newInfo.style.backgroundColor = 'rgba(0, 0, 0, 0.365)';
+          newInfo.style.width = '90%';
+          newInfo.style.height = '90%';
+          newInfo.style.color = 'white';
           var name = document.createElement("h2");
           name.id = 'nombrePlayer';
           name.innerText = obj.name;
           newInfo.appendChild(name);
-          console.log('1');
           var imagen = document.createElement("img");
           imagen.src = avatars[obj.image-1];
           imagen.className = 'characterImage';
           imagen.alt = 'characterImage';
           imagen.style.height='300px';
-          console.log('1.2');
           newInfo.appendChild(imagen);
-          console.log('2');
           var node = document.createElement("div");
           node.className = 'life';
           node.id = 'life';
-          console.log('3');
-          node.innerHTML =  `<div className="corasong"> <img src='/static/media/corazon.d3bf3074.svg' alt='vida' height='30px'/>  </div> <div class="progress"><div role="progressbar" class="progress-bar bg-danger progress-bar-animated progress-bar-striped" aria-valuenow="`+(obj.vitalpoints*2)+`" aria-valuemin="0" aria-valuemax="100" style="width: `+(obj.vitalpoints*2)+`%;"></div></div>`;
-          console.log('4');
+          node.innerHTML =  `<div className="corasong"> <img src='/static/media/corazon.d3bf3074.svg' alt='vida' height='30px'/>  </div> <div class="progress"><div role="progressbar" class="progress-bar bg-danger progress-bar-animated progress-bar-striped" aria-valuenow="`+(obj.vitalpoints*2)+`" aria-valuemin="0" aria-valuemax="100" style="width: `+(obj.vitalpoints*2)+`%;">`+(obj.vitalpoints*2)+`vp</div></div>`;
           newInfo.appendChild(node);
-          console.log('5');
           console.log(obj);
           document.getElementById("player").appendChild(newInfo);
+          
+          
+          var dire = document.getElementById("stats");
+          
+          var diversiong = document.createElement("div");
+          
+          var ataque = document.createElement("p");
+          ataque.innerText = 'Ataque: ' + obj.attack;
+          ataque.style.fontFamily = 'Lucida Console,Lucida Sans Typewriter,monaco,Bitstream Vera Sans Mono,monospace';
+          ataque.style.fontSize = '30px';
+          ataque.style.color = 'white';
+          ataque.style.marginTop = '5%';
+
+          diversiong.appendChild(ataque);
+
+          var img = document.createElement("img");
+          img.src =  espadas;
+          img.alt = 'espadas';
+          img.style.width = '10%';
+          img.style.float = 'left';
+
+          diversiong.appendChild(img);
+
+          dire.appendChild(diversiong);
+          
+          var defensa = document.createElement("p");
+          defensa.innerText = 'Defensa: ' + obj.defense;
+          defensa.style.fontFamily = 'Lucida Console,Lucida Sans Typewriter,monaco,Bitstream Vera Sans Mono,monospace';
+          defensa.style.fontSize = '30px';
+          defensa.style.color = 'white';
+          dire.appendChild(defensa);
 
         })
         .catch(function (err) {
@@ -105,7 +133,7 @@ function nuevaPartida(){
 }
 
 function eliminarJugador(){
-  if(constantes.PLAYER_INFO.player_token === "" || constantes.PLAYER_INFO.player_token === "" ){
+  if(constantes.PLAYER_INFO.player_token === "" || constantes.PLAYER_INFO.security_code === "" ){
     console.log('Crea un jugador primero!');
   }else{
     makeRequest('GET', 'http://battlearena.danielamo.info/api/remove/'+constantes.TOKEN+'/'+constantes.PLAYER_INFO.player_token+'/'+constantes.PLAYER_INFO.security_code)
@@ -200,7 +228,6 @@ function App() {
         <div className="player" id="player">
          
         </div>
-
         <div className="terminal" id="terminal" >
           <h3>TERMINAL</h3>
           <div className="log" id="log">
@@ -222,8 +249,34 @@ function App() {
           </div>
         </div>
         <div className="bruju"></div>
-        <div className="stats"></div>
-        <div className="neutral"></div>
+        <div className="stats" id="stats"></div>
+        <div className="objetos">
+          <div className="obj1">
+            <div className="imgObj1"></div>
+            <div className="infoObj1"></div>
+          </div>
+          <div className="obj2">
+            <div className="imgObj2"></div>
+            <div className="infoObj2"></div>
+          </div>
+          <div className="obj3">
+            <div className="imgObj3"></div>
+            <div className="infoObj3"></div>
+          </div>
+          <div className="obj4">
+            <div className="imgObj4"></div>
+            <div className="infoObj4"></div>
+          </div>
+          <div className="obj5">
+            <div className="imgObj5"></div>
+            <div className="infoObj5"></div>
+          </div>
+          <div className="obj6">
+            <div className="imgObj6"></div>
+            <div className="infoObj6"></div>
+          </div>
+          <div className="explicacion"></div>
+        </div>
       </header>
     </div>
   );
--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,8 @@ import left from './left.svg';
 import down from './down.svg';
 import right from './right.svg';
 import vidaLogo from './corazon.svg';
+import espadas from './espadas.svg';
+import escudo from './escudo.svg';
 
 
-export { logo, up, left, down, right, vidaLogo};
+export { logo, up, left, down, right, vidaLogo, espadas, escudo};
